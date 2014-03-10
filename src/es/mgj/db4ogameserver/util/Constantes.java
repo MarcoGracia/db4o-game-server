c34befc8ec80b2dc33f316b853a645c1881e33d0
Marco Gracia Jerez <devmgj@gmail.com>
2014-03-10 11:34:23 +0100

    First commit

---
+++ src/es/mgj/db4ogameserver/util/Constantes.java
@@ -0,0 +1,10 @@
+package es.mgj.db4ogameserver.util;
+
+public class Constantes {
+
+	public static String HOST = "localhost";
+	public static String DB4O_FILENAME = "db_game.db4o";
+	public static int PUERTO = 8090;
+	public static String USUARIO = "root";
+	public static String CONTRASENA = "58yxp1";
+}
